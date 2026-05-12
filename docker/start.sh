ac4acd619d4e0236edb01c7147350259d729f28c
Cristian <enzen544@gmail.com>
2026-05-12 18:50:04 -0500

    cambios a nginx y start

--- docker/start.sh
+++ docker/start.sh
@@ -3,15 +3,14 @@ set -e
 
 PORT="${PORT:-8080}"
 
-# Reemplazar puerto dinámico de Render
 sed -i "s/listen 8080;/listen ${PORT};/g" /etc/nginx/sites-available/default
 
 mkdir -p /var/run/php
 
-# ✅ Migraciones automáticas en cada deploy
-php /var/www/artisan migrate --force
-php /var/www/artisan config:cache
-php /var/www/artisan route:cache
-php /var/www/artisan view:cache
+# ✅ Ruta correcta
+php /var/www/laTrocha/artisan migrate --force
+php /var/www/laTrocha/artisan config:cache
+php /var/www/laTrocha/artisan route:cache
+php /var/www/laTrocha/artisan view:cache
 
 exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
