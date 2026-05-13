f6415d218e2baa51e5955aff326ad34904e3a6eb
Cristian <enzen544@gmail.com>
2026-05-12 19:07:47 -0500

    startsh

--- docker/start.sh
+++ docker/start.sh
@@ -7,7 +7,10 @@ sed -i "s/listen 8080;/listen ${PORT};/g" /etc/nginx/sites-available/default
 
 mkdir -p /var/run/php /var/log/nginx /var/log/supervisor
 
-php /var/www/laTrocha/artisan migrate --force
+chmod -R 777 /var/www/laTrocha/storage
+chmod -R 777 /var/www/laTrocha/bootstrap/cache
+
+php /var/www/laTrocha/artisan migrate:fresh --force
 php /var/www/laTrocha/artisan config:cache
 php /var/www/laTrocha/artisan route:cache
 php /var/www/laTrocha/artisan view:cache
